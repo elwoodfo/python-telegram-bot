8a7e529a56b96ef8274f70e27df72a6e360dc19c
elwoodfo <disikwindows@gmail.com>
2026-01-02 01:17:45 +0500

    Внесены новые исправления

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ from aiogram.filters import Command
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
-
 @app.route('/')
 def home():
     return "Bot is alive"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
 
 def run():
     app.run(host='0.0.0.0', port=8080)
